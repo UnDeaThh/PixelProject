6cfc7d046a318e47e5e37b3ee88248546e687bc3
joanliqui <55779010+joanliqui@users.noreply.github.com>
2019-10-25 10:20:51 +0200

    WallSlide Done

--- Sigmund's Path/Assets/Scripts/PlayerController.cs
+++ Sigmund's Path/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,10 @@ public class PlayerController : MonoBehaviour
     private float checkRadius = 0.2f;
     public LayerMask whatIsGrounded;
     private bool canJump;
+
+    //HARDFALLING DOWN
+    public float downForce;
+
     //DASH
     public float dashSpeed;
     private bool canDash;
@@ -29,6 +33,15 @@ public class PlayerController : MonoBehaviour
     private float timeDashing;
     public float dashDuration;
     private float dashDir;
+
+    //WALL SLIDING
+    public Transform wallCheckPos;
+    private float wallCheckDistance = 0.2f;
+    public float wallSlideSpeed;
+    private bool isTouchingWall;
+    private bool isWallSliding;
+    public bool wasWallSliding;
+
     void Awake(){
         rb2d = GetComponent<Rigidbody2D>();
         jumpsLeft = maxJumps;
@@ -36,6 +49,8 @@ public class PlayerController : MonoBehaviour
     }
     void Update(){
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGrounded);
+        isTouchingWall = Physics2D.Raycast(wallCheckPos.position, transform.right, wallCheckDistance, whatIsGrounded); 
+        CheckIfWallSliding();
         CheckIfCanJump();
         CheckIfCanDash();
         PlayerInput();
@@ -48,6 +63,7 @@ public class PlayerController : MonoBehaviour
         Jump();
         Dash();
         HardFallingDown();
+        IsWallSliding();
     }
 
     void PlayerInput(){
@@ -64,8 +80,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void CheckIfCanJump(){
-        if(isGrounded && rb2d.velocity.y <= 0){
+    void CheckIfCanJump()
+    {
+        if(isGrounded && rb2d.velocity.y <= 0)
+        {
             jumpsLeft = maxJumps;
         }
         if(jumpsLeft <= 0){
@@ -76,55 +94,91 @@ public class PlayerController : MonoBehaviour
         }
     }
     void Jump(){
-        if(Input.GetButtonDown("Jump")){
+        if(Input.GetButtonDown("Jump"))
+        {
             if(canJump){
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
                 jumpsLeft --;
             }
         }
     }
-    void HardFallingDown(){
-        if(jumpsLeft <= 0 && Input.GetKeyDown(KeyCode.S)){
-            Debug.Log("Hard");
+    void HardFallingDown()
+        {
+            if(jumpsLeft <= 0 && Input.GetKeyDown(KeyCode.S) && !isGrounded){
+                rb2d.velocity = (Vector2.down * downForce);
         }
     }
 
     void CheckIfCanDash(){
-        if(isGrounded){
+        if(isGrounded)
+        {
             canDash = true;
         }
-        else {
+        else
+        {
             canDash = false;
         }
     }
     
     void Dash(){
-        if(canDash){
-            if(Input.GetKeyDown(KeyCode.LeftShift) && !stopDashing){
+        if(canDash)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftShift) && !stopDashing)
+            {
                 isDashing = true;
                 dashDir = facingRight;
             }
-            if(isDashing && timeDashing >= 0f){
+            if(isDashing && timeDashing >= 0f)
+            {
                 timeDashing -= Time.fixedDeltaTime;
                 rb2d.velocity = new Vector2(dashDir * dashSpeed, rb2d.velocity.y);
             }
-            else{
+            else
+            {
                 isDashing = false;
                 timeDashing = dashDuration;
             }
-            if(timeDashing < 0){
+            if(timeDashing < 0)
+            {
                 StartCoroutine(GoNextDash());
             }
         }
     }
 
-    IEnumerator GoNextDash(){
+    IEnumerator GoNextDash()
+    {
         stopDashing = true;
         yield return new WaitForSeconds(timeTillNextDash);
         stopDashing = false;
     }
 
+    void CheckIfWallSliding()
+    {
+        if(isTouchingWall && !isGrounded)
+        {
+            isWallSliding = true;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    void IsWallSliding()
+    {
+        if(isWallSliding)
+        {
+            wasWallSliding = true;
+            if(rb2d.velocity.y < -0.1f)
+            {
+                movDir = 0;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, -wallSlideSpeed);
+            }
+        }
+    }
+
     private void OnDrawGizmos(){
         Gizmos.DrawWireSphere(feetPos.position, checkRadius);
+        Gizmos.DrawLine(wallCheckPos.position, new Vector3(wallCheckPos.position.x + wallCheckDistance, wallCheckPos.position.y, wallCheckPos.position.z));
     }
 }
