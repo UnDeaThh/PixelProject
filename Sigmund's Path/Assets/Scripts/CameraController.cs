19f2f9d90ed4e998e510afad870b948196e4b065
joanliqui <joanliqui@gmail.com>
2019-12-13 12:08:54 +0100

    haciendo camera shake

---
+++ Sigmund's Path/Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Events;
+public class CameraController : MonoBehaviour
+{
+    public float shakeDuration = 0.3f;          // Time the Camera Shake effect will last
+    public float shakeAmplitude = 1.2f;         // Cinemachine Noise Profile Parameter
+    public float shakeFrequency = 2.0f;
+
+    private float shakeCurrentTime = 0f;
+
+    // Cinemachine Shake
+    public CinemachineVirtualCamera VirtualCamera;
+    private CinemachineBasicMultiChannelPerlin virtualCameraNoise;
+    void Start()
+    {
+        if (VirtualCamera != null)
+            virtualCameraNoise = VirtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            shakeCurrentTime = shakeDuration;
+        }
+
+        if (VirtualCamera != null && virtualCameraNoise != null)
+        {
+            // If Camera Shake effect is still playing
+            if (shakeCurrentTime > 0)
+            {
+                // Set Cinemachine Camera Noise parameters
+                virtualCameraNoise.m_AmplitudeGain = shakeAmplitude;
+                virtualCameraNoise.m_FrequencyGain = shakeFrequency;
+
+                // Update Shake Timer
+                shakeCurrentTime -= Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            // If Camera Shake effect is over, reset variables
+            virtualCameraNoise.m_AmplitudeGain = 0f;
+            shakeCurrentTime = 0f;
+        }
+    }
+}
