3fcf3b34a6ed7df7d92e0e1bb8c98ecd7de3e9ad
Andrea Bermudez <andreabermudez_bcnvj1ma1920@cev.com>
2020-01-13 12:26:58 +0100

    nada

--- Sigmund's Path/Assets/Scripts/Shop/ShopController.cs
+++ Sigmund's Path/Assets/Scripts/Shop/ShopController.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
+    public List<ItemHolderButton> holderButtons = new List<ItemHolderButton>();
 
+    public GameObject holderPrefab;
+    public static ShopController shopController;
+
+    public Transform content;
+
+    private void Awake()
+    {
+        shopController = this;
+        FillList();
+    }
+
+    void FillList()
+    {
+        for (int i = 0; i < holderButtons.Count; i++)
+        {
+            GameObject holder = Instantiate(holderPrefab, content);
+        }
+    }
 }
