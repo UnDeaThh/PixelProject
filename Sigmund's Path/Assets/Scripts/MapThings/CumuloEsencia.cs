ce6b6770c36e9c767915419cd41b410c9e4427ae
joanliqui <joanliqui@gmail.com>
2020-04-14 12:54:10 +0200

    Revert "as"
    This reverts commit db0b7195cf341ee1924befcb1edccf7c9446ccf9.

--- Sigmund's Path/Assets/Scripts/MapThings/CumuloEsencia.cs
+++ Sigmund's Path/Assets/Scripts/MapThings/CumuloEsencia.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Experimental.Rendering.LWRP;
 
 public class CumuloEsencia : MonoBehaviour
 {
