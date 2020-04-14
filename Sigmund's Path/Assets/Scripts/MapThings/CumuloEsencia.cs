db0b7195cf341ee1924befcb1edccf7c9446ccf9
joanliqui <joanliqui@gmail.com>
2020-04-14 12:54:06 +0200

    as

--- Sigmund's Path/Assets/Scripts/MapThings/CumuloEsencia.cs
+++ Sigmund's Path/Assets/Scripts/MapThings/CumuloEsencia.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class CumuloEsencia : MonoBehaviour
 {
