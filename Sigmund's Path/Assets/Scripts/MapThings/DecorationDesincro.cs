669e9347c7f05dd490c03f3a1c77aa720fcbd359
joanliqui <joanliqui@gmail.com>
2020-06-02 20:18:45 +0200

    RanomStart props anims

---
+++ Sigmund's Path/Assets/Scripts/MapThings/DecorationDesincro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class DecorationDesincro : MonoBehaviour
+{
+
+    private Animator anim;
+   
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        float randomStart = Random.Range(0, 1f);
+        anim.SetFloat("randomStart", randomStart);
+    }
+
+}
