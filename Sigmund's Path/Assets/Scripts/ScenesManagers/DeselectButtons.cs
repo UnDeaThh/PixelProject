5ff34222a0757f93ac6237250bcec464698151e6
joanliqui <joanliqui@gmail.com>
2020-06-20 00:16:51 +0200

    button navigation main menu

---
+++ Sigmund's Path/Assets/Scripts/ScenesManagers/DeselectButtons.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class DeselectButtons : MonoBehaviour
+{
+    [SerializeField] List<Button> buttons = new List<Button>();
+    private EventSystem eventSystem;
+    [SerializeField] GameObject firstSelected;
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).TryGetComponent<Button>(out Button boton))
+            {
+                buttons.Add(boton);
+            }
+        }
+
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+
+        if(ScenesManager.scenesManager.Gamepad != null)
+        {
+            eventSystem.SetSelectedGameObject(firstSelected);
+            Debug.Log("asas");
+        }
+        else
+        {
+            eventSystem.SetSelectedGameObject(null);
+        }
+    }
+
+    private void Update()
+    {
+        InputSystem.onDeviceChange +=
+            (device, change) =>
+            {
+                switch (change)
+                {
+                    case InputDeviceChange.Added:
+                        eventSystem.SetSelectedGameObject(firstSelected);
+                        break;
+                    case InputDeviceChange.Removed:
+                        eventSystem.SetSelectedGameObject(null);
+                        break;
+                }
+            };
+    }
+}
